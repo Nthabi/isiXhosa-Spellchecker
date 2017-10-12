986817a1c0f4a5aa46e3fa7f03d56bdc6a6df547
Nthabi <nthabimashiane@gmail.com>
2017-10-12 03:37:14 +0200

    Working product v1 commit

---
+++ IsiXhosa_Spellchecker/src/IsiXhosa_spellchecker/IsiXhosa_Spellchecker.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IsiXhosa_spellchecker;
+/**
+ *
+ * @author Nthabiseng Mashiane
+ */
+
+public class IsiXhosa_Spellchecker {
+
+    public static void main(String[] args){
+        
+        //Spellchecker.main(args);
+        AccuracyTest ac = new AccuracyTest();
+        ac.main(args);
+    }
+
+}
